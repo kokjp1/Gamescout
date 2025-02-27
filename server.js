08cb72ba19f683eb8b65e4f5cb2363ff11af3c1f
kokjp1 <james.kok@hva.nl>
2025-02-27 16:14:29 +0100

    nothing

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
 app.use(express.urlencoded({extended: true})) 
 app.use(express.static('static'))             
 app.set('view engine', 'ejs')                 
