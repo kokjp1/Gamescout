9ba161ab55ea619020839b2439f006507d1cca75
kokjp1 <james.kok@hva.nl>
2025-03-03 12:14:40 +0100

    Inlog functionaliteit upload

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express()
 app.use(express.urlencoded({extended: true})) 
 app.use(express.static('static'))             
 app.set('view engine', 'ejs')                 
-app.set('views', 'views')
+app.set('views', 'views'
+
+)
 
 
 
@@ -43,6 +45,60 @@ client.connect()
   app.get('/', (req, res) => {
   res.render('index.ejs')
 })
+
+  app.get('/login', onlogin);
+  app.get('/register', onregister);
+
+
+  app.post('/login', authdoorsturen)
+
+
+async function authdoorsturen (req, res) {
+  
+  const ingevuldeEmailadress = req.body.email;
+  const ingevuldePassword = req.body.password;
+
+  const account = await activeCollection.findOne({ email: ingevuldeEmailadress });
+  // checken of er een object/acc is met het emailadres, zo ja, dan slaat hij HET HELE OBJECT (inc. password) op in "account"
+  // als ie geen account kan vinden met het emailadres, dan is account waarde undefined (en dus falsy)
+
+    if (!account) {
+      return res.render('login.ejs', { errorMessageEmail: 'We kunnen geen account vinden met dit emailadres', errorMessagePassword: '' });
+    }
+
+    if (account.password === ingevuldePassword) {
+      return res.send('<img src="https://media.tenor.com/Ex-Vvbuv2DQAAAAM/happy-birthday-celebrate.gif">');
+    } 
+
+    else {
+      return res.render('login.ejs', { errorMessageEmail: '', errorMessagePassword: 'Onjuist wachtwoord, probeer het opnieuw of klik op wachtwoord vergeten' });
+    }
+}
+
+function onlogin(req, res) {
+  res.render('login.ejs' , { errorMessageEmail:'', errorMessagePassword:'' });	
+}
+
+app.post('/register', registreertoevoegen);
+
+async function registreertoevoegen(req, res) {
+  const RegisteringEmail = req.body.email;
+  const RegisteringPassword = req.body.password;
+
+  toegevoegdeaccount = await activeCollection.insertOne({
+    email: RegisteringEmail,
+    password: RegisteringPassword
+  })  
+
+  console.log(`added account to database with _id: ${toegevoegdeaccount.insertedId}`)
+  res.send('account toegevoegd')
+
+}
+
+function onregister(req, res) {
+  res.render('register.ejs');
+}
+
 
 // error handlers - **ALTIJD ONDERAAN HOUDEN**
 
