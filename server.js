8209c6f37e2fef3af2be16cddb2726660e576fbc
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-03-10 14:24:23 +0100

    sessions

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const helmet = require("helmet");
 const express = require("express");
 const app = express();
+const session = require('express-session')
 
 // app.use(xss());
 
@@ -14,6 +15,29 @@ app.use(express.static("static"));
 app.use(helmet());
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
+
+
+
+
+
 
 // MongoDB setup
 
