def33c7701ae3bd02ade360f29d899d3bda30c16
BrahamjeetRandhawa <152489943+BrahamjeetRandhawa@users.noreply.github.com>
2025-03-13 12:39:00 +0100

    Helmloos

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 require("dotenv").config();
 const xss = require("xss");
 const bcrypt = require("bcrypt");
-const helmet = require("helmet");
 const express = require("express");
 const app = express();
 const session = require("express-session");
@@ -25,7 +24,6 @@ app.use(
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
-app.use(helmet());
 app.set("view engine", "ejs");
 app.set("views", "views");
 
