6cb3cb04921a88bf5c648042b8c9ad31ac8cea76
kokjp1 <james.kok@hva.nl>
2025-03-17 15:52:55 +0100

    fixed css file extension and duplicate objectID

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set("views", "views");
 
 // MongoDB setup
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const mongoDBtoken = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const { ObjectId } = require("mongodb");
