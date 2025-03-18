3363646e7cafbc81c1b3e05833264b4fbe3b95f8
kokjp1 <james.kok@hva.nl>
2025-03-18 22:31:43 +0100

    worked on API Function for gameform

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ app.get("/games", async (req, res) => {
 app.post("/gameFinderForm", gameFormHandler);
 
 async function gameFormHandler(req, res) {
-  const { release_date, tags, platform, multiplayer, noLimit } = req.body;
+  const { release_date, genre, platform, multiplayerSingleplayer , noLimit } = req.body;
 
   let gameReleaseDate;
 
@@ -262,25 +262,26 @@ async function gameFormHandler(req, res) {
   } else {
     gameReleaseDate = `${release_date}-01-01,${release_date}-12-31`;
   }
-
-    const gameTags = tags.join(",");
+    const gameGenres = genre.join(",");
     const gamePlatform = platform;
-    const gameMultiplayer = multiplayer;
+    const gameMultiplayerSingleplayer = multiplayerSingleplayer;
     const apiKey = process.env.API_KEY;
 
-    console.log("Fetching games for:", gameReleaseDate, gameTags, gamePlatform, gameMultiplayer);
+    console.log("Fetching games for:", gameReleaseDate, gameGenres, gamePlatform, gameMultiplayerSingleplayer);
 
     const response = await fetch(
-        `https://api.rawg.io/api/games?key=${apiKey}&dates=${gameReleaseDate}&tags=${gameTags}&platforms=${gamePlatform}&multiplayer=${gameMultiplayer}`
+        `https://api.rawg.io/api/games?key=${apiKey}&dates=${gameReleaseDate}&genres=${gameGenres}&platforms=${gamePlatform}&tags=${gameMultiplayerSingleplayer}&search_precise=true`
+        // eventueel achteraan nog &search_precise=true zetten
     );
 
     const data = await response.json();
     
-    console.log(data.results)
-    console.log(`https://api.rawg.io/api/games?key=${apiKey}&dates=${gameReleaseDate}&tags=${gameTags}&platforms=${gamePlatform}&multiplayer=${gameMultiplayer}`);
+    // console.log(data.results)
+    console.log(`https://api.rawg.io/api/games?key=${apiKey}&dates=${gameReleaseDate}&genres=${gameGenres}&platforms=${gamePlatform}&multiplayer=${gameMultiplayerSingleplayer}`);
 
     res.render("results.ejs", { games: data.results });
 };
+
 // De Query Paramters moeten juist benoemd worden, "multiplayer is bijv. geen optie maar een tag net als battle-royale. Daarnaast moet ook uitgezocht worden hoe 
 // het platform nou echt in de frontend gezet moet worden zodat het in de backend werkt "
 
