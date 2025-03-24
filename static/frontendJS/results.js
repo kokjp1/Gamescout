e3e3435d9b8fcd84205c3debdf8dbce9f29ae0dc
kokjp1 <james.kok@hva.nl>
2025-03-24 15:25:22 +0100

    started designing and working the results page

--- static/frontendJS/results.js
+++ static/frontendJS/results.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", function () {
     valueNames: ["name", "released", "rating"],
   };
   var gameList = new List("game-list", options);
+
+  // Sort buttons
+  document.getElementById("sort-ascending").addEventListener("click", function () {
+    gameList.sort("name", { order: "asc" });
+  });
+
+  document.getElementById("sort-descending").addEventListener("click", function () {
+    gameList.sort("name", { order: "desc" });
+  });
 });
